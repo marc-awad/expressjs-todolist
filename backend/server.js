ebd8294905c85a4c46a2cb7633af41e357dd48df
Marc AWAD <marc.awad@supdevinci-edu.fr>
2025-01-25 15:44:33 +0100

    Enflying the server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,30 @@
-//Fichier de base pour les routes
+// Base file for routes
 const express = require("express")
 const jsonServer = require("json-server")
 
 const PORT = 3001
 
-// Initialisation d'Express
+// Initialize Express
 const app = express()
 
-// Middleware pour lire le JSON dans le body des requêtes POST/PUT/PATCH
+// Middleware to read JSON in the body of POST/PUT/PATCH requests
 app.use(express.json())
 
-// Création du routeur json-server basé sur le fichier db.json
+// Create the json-server router based on the db.json file
 const router = jsonServer.router("./db.json")
 const advancedTaskRoutes = require("./routes/advancedTaskRoutes")
 
-// Middlewares par défaut de json-server (logs, CORS, etc.)
+// Default middlewares for json-server (logs, CORS, etc.)
 const middlewares = jsonServer.defaults()
 
-// On utilise ces middlewares
+// Use these middlewares
 app.use(middlewares)
-
 
 app.use("/db", router)
 
 app.use("/api/advanced/tasks", advancedTaskRoutes)
 
-
-// Démarrage du serveur
+// Start the server
 app.listen(PORT, () => {
-  console.log(`Serveur démarré sur le port ${PORT} : http://localhost:${PORT}/`)
+  console.log(`Server started on port ${PORT}: http://localhost:${PORT}/`)
 })
